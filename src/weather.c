e769e570a88401aacfd3425fd0b5364e3ed5e3f8
Chris Lewis <Bonsitm@gmail.com>
2015-10-29 13:55:42 -0700

    Tidy up main C file and fix layout on Chalk

--- src/weather.c
+++ src/weather.c
@@ -10,17 +10,17 @@ static GBitmap *s_icon_bitmap = NULL;
 static AppSync s_sync;
 static uint8_t s_sync_buffer[64];
 
-enum WeatherKey {
-  WEATHER_ICON_KEY = 0x0,         // TUPLE_INT
-  WEATHER_TEMPERATURE_KEY = 0x1,  // TUPLE_CSTRING
-  WEATHER_CITY_KEY = 0x2,         // TUPLE_CSTRING
-};
+typedef enum {
+  WeatherKeyIcon = 0,
+  WeatherKeyTemperature,
+  WeatherKeyCity,       
+} WeatherKey;
 
 static const uint32_t WEATHER_ICONS[] = {
-  RESOURCE_ID_IMAGE_SUN, // 0
-  RESOURCE_ID_IMAGE_CLOUD, // 1
-  RESOURCE_ID_IMAGE_RAIN, // 2
-  RESOURCE_ID_IMAGE_SNOW // 3
+  RESOURCE_ID_IMAGE_SUN,    // 0
+  RESOURCE_ID_IMAGE_CLOUD,  // 1
+  RESOURCE_ID_IMAGE_RAIN,   // 2
+  RESOURCE_ID_IMAGE_SNOW    // 3
 };
 
 static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
@@ -29,26 +29,20 @@ static void sync_error_callback(DictionaryResult dict_error, AppMessageResult ap
 
 static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
   switch (key) {
-    case WEATHER_ICON_KEY:
+    case WeatherKeyIcon:
       if (s_icon_bitmap) {
         gbitmap_destroy(s_icon_bitmap);
       }
-
       s_icon_bitmap = gbitmap_create_with_resource(WEATHER_ICONS[new_tuple->value->uint8]);
-
-#ifdef PBL_SDK_3
-      bitmap_layer_set_compositing_mode(s_icon_layer, GCompOpSet);
-#endif
-
       bitmap_layer_set_bitmap(s_icon_layer, s_icon_bitmap);
       break;
 
-    case WEATHER_TEMPERATURE_KEY:
+    case WeatherKeyTemperature:
       // App Sync keeps new_tuple in s_sync_buffer, so we may use it directly
       text_layer_set_text(s_temperature_layer, new_tuple->value->cstring);
       break;
 
-    case WEATHER_CITY_KEY:
+    case WeatherKeyCity:
       text_layer_set_text(s_city_layer, new_tuple->value->cstring);
       break;
   }
@@ -58,15 +52,11 @@ static void request_weather(void) {
   DictionaryIterator *iter;
   app_message_outbox_begin(&iter);
 
-  if (!iter) {
-    // Error creating outbound message
-    return;
-  }
+  const int dummy_key = 1;
+  const int dummy_value = 1;
+  dict_write_int(iter, dummy_key, &dummy_value, sizeof(int), true);
 
-  int value = 1;
-  dict_write_int(iter, 1, &value, sizeof(int), true);
   dict_write_end(iter);
-
   app_message_outbox_send();
 }
 
@@ -74,29 +64,33 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_icon_layer = bitmap_layer_create(GRect(32, 10, 80, 80));
+  s_icon_layer = bitmap_layer_create(GRect(0, 10, bounds.size.w, 80));
+#ifdef PBL_SDK_3
+  bitmap_layer_set_compositing_mode(s_icon_layer, GCompOpSet);
+#endif
   layer_add_child(window_layer, bitmap_layer_get_layer(s_icon_layer));
 
   s_temperature_layer = text_layer_create(GRect(0, 90, bounds.size.w, 32));
-  text_layer_set_text_color(s_temperature_layer, COLOR_FALLBACK(GColorBlack, GColorWhite));
+  text_layer_set_text_color(s_temperature_layer, PBL_IF_COLOR_ELSE(gcolor_legible_over(GColorIndigo), GColorWhite));
   text_layer_set_background_color(s_temperature_layer, GColorClear);
   text_layer_set_font(s_temperature_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   text_layer_set_text_alignment(s_temperature_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_temperature_layer));
 
   s_city_layer = text_layer_create(GRect(0, 122, bounds.size.w, 32));
-  text_layer_set_text_color(s_city_layer, COLOR_FALLBACK(GColorBlack, GColorWhite));
+  text_layer_set_text_color(s_city_layer, PBL_IF_COLOR_ELSE(gcolor_legible_over(GColorIndigo), GColorWhite));
   text_layer_set_background_color(s_city_layer, GColorClear);
   text_layer_set_font(s_city_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   text_layer_set_text_alignment(s_city_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_city_layer));
 
   Tuplet initial_values[] = {
-    TupletInteger(WEATHER_ICON_KEY, (uint8_t) 1),
-    TupletCString(WEATHER_TEMPERATURE_KEY, "1234\u00B0C"),
-    TupletCString(WEATHER_CITY_KEY, "St Pebblesburg"),
+    TupletInteger(WeatherKeyIcon, (uint8_t) 1),
+    TupletCString(WeatherKeyTemperature, "1234\u00B0C"),
+    TupletCString(WeatherKeyCity, "St Pebblesburg"),
   };
 
+  app_message_open(64, 64);
   app_sync_init(&s_sync, s_sync_buffer, sizeof(s_sync_buffer),
       initial_values, ARRAY_LENGTH(initial_values),
       sync_tuple_changed_callback, sync_error_callback, NULL);
@@ -116,7 +110,7 @@ static void window_unload(Window *window) {
 
 static void init(void) {
   s_main_window = window_create();
-  window_set_background_color(s_main_window, COLOR_FALLBACK(GColorIndigo, GColorBlack));
+  window_set_background_color(s_main_window, PBL_IF_COLOR_ELSE(GColorIndigo, GColorBlack));
 
 #ifdef PBL_SDK_2
   window_set_fullscreen(s_main_window, true);
